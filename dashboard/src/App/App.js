d5ec92bc405241ac0116c4c34dffdef725fad6b4
igorcaldeira <igorcaldeiraa@gmail.com>
2020-04-11 23:56:43 -0300

    New dashboard

---
+++ dashboard/src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Nav from '../Nav';
+import MainDashboard from '../MainDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <MainDashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ dashboard/src/MainDashboard/MainDashboard.js
@@ -0,0 +1,207 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import {
+    Container,
+    Row,
+    Col,
+    Card,
+    CardHeader,
+    CardTitle,
+    CardImg,
+    CardBody,
+    CardFooter,
+    Button
+} from "shards-react";
+
+import {
+    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+  } from 'recharts';
+  
+  const chart = [
+    {
+      name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+    },
+    {
+      name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+    },
+    {
+      name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+    },
+    {
+      name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+    },
+    {
+      name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+    },
+    {
+      name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+    },
+    {
+      name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+    },
+  ];
+
+const MainDashboard = () => {
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState(null);
+    
+    const instance = axios.create({
+        baseURL: 'http://localhost:3000/',
+        timeout: 1000,
+        headers: {'X-Custom-Header': 'foobar'}
+      });
+
+    useEffect(() => {
+        setLoading(true);
+
+        instance.get('/info')
+            .then(function (response) {
+            setData(response.data);
+            })
+            .catch(function (error) {
+                setData(null);
+            console.log(error);
+            })
+            .then(function () {
+                setLoading(false);
+            });
+    }, []);
+
+    const chartFormat = data && !loading && Object.keys(data.groupByInitiatorType).map(iniType => (
+        {
+            iniType,
+            ...data.groupByInitiatorType[iniType],
+        }
+    ));
+
+    return (
+        <Container className='pt-4'>
+            {data && !loading && <>
+            <Row className='pt-3'>
+                <Col>
+                    <h3>Average time</h3>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Card className='mb-3 mt-3'>
+                        <CardBody>
+                            <CardTitle>Request (ms)</CardTitle>
+                            {data.avgTimeDuration}
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card className='mb-3 mt-3'>
+                        <CardBody>
+                            <CardTitle>Redirect (ms)</CardTitle>
+                            {data.avgTimeRedirect}
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card className='mb-3 mt-3'>
+                        <CardBody>
+                            <CardTitle>Response (ms)</CardTitle>
+                            {data.avgTimeResponse}
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className='pt-5 mt-3 pb-3'>
+                <Col>
+                    <h3>Initiator type relation</h3>
+                </Col>
+            </Row>
+            <Row className='pt-3'>
+                <Col>
+                    <Card className='mb-3 mt-3'>
+                        
+            <Row className='pt-3'>
+                <CardBody>
+            <Row className='pt-3'>
+                <Col>
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={chartFormat}
+                        margin={{
+                            top: 5, right: 30, left: 20, bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="iniType" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="avgTimeDuration" fill="#17C671" />
+                    </BarChart>
+                </Col>
+                <Col>
+                            <BarChart
+                                width={500}
+                                height={300}
+                                data={chartFormat}
+                                margin={{
+                                    top: 5, right: 30, left: 20, bottom: 5,
+                                }}
+                            >
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="iniType" />
+                                <YAxis />
+                                <Tooltip />
+                                <Legend />
+                                <Bar dataKey="avgTimeResponse" fill="#FFB400" />
+                            </BarChart>
+                </Col>
+                </Row>
+                        </CardBody>
+                </Row>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className='pt-5 mt-3 pb-3'>
+                <Col>
+                    <h3>Overview by initiator type</h3>
+                </Col>
+            </Row>
+            {
+                Object.keys(data.groupByInitiatorType)
+                    .map((initiatorType => (
+                        <>
+                        <h5 style={{ color: 'gray' }}>{initiatorType}</h5>
+                        <Row>
+                            <Col>
+                                <Card className='mb-3 mt-3'>
+                                    <CardBody>
+                                        <CardTitle>Request (ms)</CardTitle>
+                                        {data.groupByInitiatorType[initiatorType].avgTimeDuration}
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                            <Col>
+                                <Card className='mb-3 mt-3'>
+                                    <CardBody>
+                                        <CardTitle>Redirect (ms)</CardTitle>
+                                        {data.groupByInitiatorType[initiatorType].avgTimeRedirect}
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                            <Col>
+                                <Card className='mb-3 mt-3'>
+                                    <CardBody>
+                                        <CardTitle>Response (ms)</CardTitle>
+                                        {data.groupByInitiatorType[initiatorType].avgTimeResponse}
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                        </Row>
+                        </>
+                )))
+            }
+            </>}
+        </Container>
+    )
+}
+
+export default MainDashboard;
