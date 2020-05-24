005a7a6a2cf7352117f9f3c0711cad4f68ecd118
Igor Caldeira <igorcaldeiraa@gmail.com>
2020-05-24 11:30:00 -0300

    sample app build

---
+++ sample-app/build/precache-manifest.85d6fae0b1c07b20c608e49ca95a2018.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b36983a0960a17ee648eb7792b4f368a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fd96a74af563f874287",
+    "url": "/static/css/main.bc8e007b.chunk.css"
+  },
+  {
+    "revision": "e1ab7dde385b2c51e483",
+    "url": "/static/js/2.bcb9e838.chunk.js"
+  },
+  {
+    "revision": "296362f3435e3c4e1832bddadf6a9199",
+    "url": "/static/js/2.bcb9e838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd96a74af563f874287",
+    "url": "/static/js/main.eeef622f.chunk.js"
+  },
+  {
+    "revision": "6bbb9ebcc2aba687a14a",
+    "url": "/static/js/runtime-main.a57cc756.js"
+  }
+]);
