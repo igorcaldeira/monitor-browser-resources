ef3f23f994020f0ee9555e72635d4428a2615b4f
Igor Caldeira <igorcaldeiraa@gmail.com>
2020-05-24 11:36:05 -0300

    dashboar build

---
+++ dashboard/build/precache-manifest.8a3fdf0c379ced9456e01186a72d4e44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5854aa924e7d47aa5b6964d2241ff0f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36ae7e65e44a2376af4d",
+    "url": "/static/css/2.596724bf.chunk.css"
+  },
+  {
+    "revision": "10d8ac7fbbe5e28a409c",
+    "url": "/static/css/main.0e36a263.chunk.css"
+  },
+  {
+    "revision": "36ae7e65e44a2376af4d",
+    "url": "/static/js/2.4420a726.chunk.js"
+  },
+  {
+    "revision": "77ca4c830e21307762e7046c8772f074",
+    "url": "/static/js/2.4420a726.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d8ac7fbbe5e28a409c",
+    "url": "/static/js/main.e432669e.chunk.js"
+  },
+  {
+    "revision": "762f90a91cde268a7a70",
+    "url": "/static/js/runtime-main.7e703d23.js"
+  }
+]);
